92bc0e182a3ccb4408f249059650fdaaa7bd6877
zhangyunxiao <qiaoxinbo@jianzhiss.com>
2021-07-14 09:58:49 +0800

    zz

---
+++ Classes/UIView+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIView+Extension.swift
+//  Componentization
+//
+//  Created by zyx on 2021/7/14.
+//
+
+import UIKit
+
+extension UIView{
+    func viewFromXib () ->UIView{
+        
+        UIView.init();
+    }
+}
